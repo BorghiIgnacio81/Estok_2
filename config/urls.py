8f1a2358b90ddd9e1f2a7c3434619806184c244e
BorghiIgnacio81 <borghi.ignacio.sd@gmail.com>
2026-06-15 00:43:59 -0300

    Fix: redirigir favicon.ico desde Django (gunicorn) al frontend

--- config/urls.py
+++ config/urls.py
@@ -15,10 +15,11 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, include, re_path
 from django.conf import settings
 from django.conf.urls.static import static
 from django.utils.safestring import mark_safe
+from django.views.generic.base import RedirectView
 from rest_framework_simplejwt.views import (
     TokenObtainPairView,
     TokenRefreshView,
@@ -37,6 +38,9 @@ urlpatterns = [
     path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     path('api/token/verify/', TokenVerifyView.as_view(), name='token_verify'),
+    # Favicon: redirigir al frontend
+    re_path(r'^favicon\.ico$', RedirectView.as_view(url='https://eeestok.duckdns.org/favicon.png', permanent=True)),
+    re_path(r'^favicon\.png$', RedirectView.as_view(url='https://eeestok.duckdns.org/favicon.png', permanent=True)),
 ]
 
 # Servir archivos multimedia en desarrollo
